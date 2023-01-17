4ae7ae838466694f7db7e8e6e9fea22e9697c72f
FLikeSet <Stelzf117@gmail.com>
2023-01-17 10:00:12 +0500

    fix: исправить вёрстку OrderDetails

--- src/components/order-details/order-details.js
+++ src/components/order-details/order-details.js
@@ -6,16 +6,18 @@ import doneImage from '../../images/done.jpg'
 const OrderDetails = React.memo(({ popupClose }) => {
   const { inner, text1, text2, text3, done, digits, close } = styles;
   return (
-    <div className={ inner }>
+    <>
       <button className={ close } onClick={ popupClose } >
         <CloseIcon type="primary" />
       </button>
+    <div className={ inner }>
       <p className={ digits }>034536</p>
       <p className={ text1 }>идентификатор заказа</p>
       <img className={ done } src={ doneImage } alt='done' />
       <p className={ text2 }>ваш заказ начали готовить</p>
       <p className={ text3 }>дождитесь готовности на орбитальной станции</p>
     </div>
+    </>
   )
 })
 
