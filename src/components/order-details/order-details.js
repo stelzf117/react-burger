66965bc4bc0938ce0203b8098ea43a25217eb618
FLikeSet <Stelzf117@gmail.com>
2023-02-07 02:43:08 +0500

    fix: удалить closeIcon

--- src/components/order-details/order-details.js
+++ src/components/order-details/order-details.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from '../../styles/order-details.module.css'
 import doneImage from '../../images/done.jpg'
 
-const OrderDetails = React.memo(({ popupClose }) => {
-  const { inner, text1, text2, text3, done, digits, close } = styles;
+const OrderDetails = React.memo(() => {
+  const { inner, text1, text2, text3, done, digits } = styles;
   return (
-    <>
-      <button className={ close } onClick={ popupClose } >
-        <CloseIcon type="primary" />
-      </button>
     <div className={ inner }>
       <p className={ digits }>034536</p>
       <p className={ text1 }>идентификатор заказа</p>
@@ -17,7 +12,6 @@ const OrderDetails = React.memo(({ popupClose }) => {
       <p className={ text2 }>ваш заказ начали готовить</p>
       <p className={ text3 }>дождитесь готовности на орбитальной станции</p>
     </div>
-    </>
   )
 })
 
